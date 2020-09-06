d04f2d5e8d60dd17b98284ca00ef1156be6b0a1b
Jeron Aldaron Lau <jeronlau@plopgrizzly.com>
2020-09-05 22:43:59 -0500

    Add press() and release() APIs

--- kbrd/src/lib.rs
+++ kbrd/src/lib.rs
@@ -1,5 +1,26 @@
 pub mod keycodes;
 
+use keycodes::Mods;
+
+/// Process pressing a key.
+pub fn press<K: Into<u8>, F: FnMut(&Mods, u8)>(mods: &mut Mods, keycode: K, mut process: F) {
+    if let Some(events) = keycodes::MAPPINGS[keycode.into() as usize] {
+        for (key, modifiers) in events.iter().cloned() {
+            mods.set(modifiers);
+            process(mods, key.into());
+        }
+    }
+}
+/// Process releasing a key.
+pub fn release<K: Into<u8>, F: FnMut(&Mods, u8)>(mods: &mut Mods, keycode: K, mut process: F) {
+    if let Some(events) = keycodes::MAPPINGS[keycode.into() as usize] {
+        for (key, modifiers) in events.iter().cloned() {
+            mods.clear(modifiers);
+            process(mods, key.into());
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
